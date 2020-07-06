ee19d3b5f54bc89551247895ea1c4f84860bd8ca
Bhanu Agrawal <bhanuagrawal2018@gmail.com>
2020-07-06 22:23:53 +0530

    Added wget install command

--- pre-build-win.sh
+++ pre-build-win.sh
@@ -7,7 +7,8 @@ mkdir -p temp
 # if not exist "temp" mkdir temp
 cd temp && rm -rf win
 mkdir win && cd win
-curl https://github.com/DeFiCh/ain/releases/download/v1.0.0-rc1/defichain-1.0.0-rc1-x86_64-w64-mingw32.zip -O defichain-1.0.0-rc1-x86_64-w64-mingw32.zip
+apt-get install wget -y
+wget https://github.com/DeFiCh/ain/releases/download/v1.0.0-rc1/defichain-1.0.0-rc1-x86_64-w64-mingw32.zip
 unzip defichain-1.0.0-rc1-x86_64-w64-mingw32.zip
 # unzip -o "defichain-1.0.0-rc1-x86_64-w64-mingw32.zip" "defichain-1.0.0-rc1-x86_64-w64-mingw32.zip" 
 copy defichain-1.0.0-rc1/bin/defid.exe .
