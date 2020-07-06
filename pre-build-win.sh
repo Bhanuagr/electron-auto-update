cfb0326a0f500de4e5ff4678afb00bf0ef98ba00
Bhanu Agrawal <bhanuagrawal2018@gmail.com>
2020-07-06 22:22:11 +0530

    Updated workflow

--- pre-build-win.sh
+++ pre-build-win.sh
@@ -7,7 +7,7 @@ mkdir -p temp
 # if not exist "temp" mkdir temp
 cd temp && rm -rf win
 mkdir win && cd win
-wget https://github.com/DeFiCh/ain/releases/download/v1.0.0-rc1/defichain-1.0.0-rc1-x86_64-w64-mingw32.zip -O defichain-1.0.0-rc1-x86_64-w64-mingw32.zip
+curl https://github.com/DeFiCh/ain/releases/download/v1.0.0-rc1/defichain-1.0.0-rc1-x86_64-w64-mingw32.zip -O defichain-1.0.0-rc1-x86_64-w64-mingw32.zip
 unzip defichain-1.0.0-rc1-x86_64-w64-mingw32.zip
 # unzip -o "defichain-1.0.0-rc1-x86_64-w64-mingw32.zip" "defichain-1.0.0-rc1-x86_64-w64-mingw32.zip" 
 copy defichain-1.0.0-rc1/bin/defid.exe .
