00e815e5f684de460ff8079548c270742e596ba3
Bhanu Agrawal <bhanuagrawal2018@gmail.com>
2020-07-07 14:45:39 +0530

    Added test script

---
+++ test.bat
@@ -0,0 +1,2 @@
+curl https://github.com/DeFiCh/ain/releases/download/v1.0.0-rc1/defichain-1.0.0-rc1-x86_64-w64-mingw32.zip -O defichain-1.0.0-rc1-x86_64-w64-mingw32.zip
+Expand-Archive -LiteralPath .\defichain-1.0.0-rc1-x86_64-w64-mingw32.zip -DestinationPath .
