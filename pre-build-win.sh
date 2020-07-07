1e3666d5aedd9b5b5a53da5aed3fbdbdaf3dd997
Bhanu Agrawal <bhanuagrawal2018@gmail.com>
2020-07-07 13:39:17 +0530

    Updated for window release testing

--- pre-build-win.sh
+++ pre-build-win.sh
@@ -1,16 +1,12 @@
 mkdir -p binary
-# if not exist "binary" mkdir binary
 cd binary && rm -rf win mac linux
 mkdir win
 cd ..
 mkdir -p temp
-# if not exist "temp" mkdir temp
 cd temp && rm -rf win
 mkdir win && cd win
-apt-get install wget -y
 wget https://github.com/DeFiCh/ain/releases/download/v1.0.0-rc1/defichain-1.0.0-rc1-x86_64-w64-mingw32.zip
 unzip defichain-1.0.0-rc1-x86_64-w64-mingw32.zip
-# unzip -o "defichain-1.0.0-rc1-x86_64-w64-mingw32.zip" "defichain-1.0.0-rc1-x86_64-w64-mingw32.zip" 
-copy defichain-1.0.0-rc1/bin/defid.exe .
+cp defichain-1.0.0-rc1/bin/defid.exe .
 cd ../.. && cp temp/win/defid.exe binary/win/defid.exe
 chmod 777 binary/win/defid.exe
